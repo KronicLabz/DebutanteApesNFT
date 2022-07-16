739d5a78a6a090c37aa02499ef83ff9a80b48e8d
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-07-16 02:33:28 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,354 @@
+import React, { useEffect, useState } from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import myEpicNft from './utils/MyEpicNFT.json';
+import nftGif from './assets/nft.gif';
+import './styles/App.css';
+
+import { ethers } from 'ethers';
+import { FaWallet, FaCheck, FaRegGem, FaThLarge } from 'react-icons/fa';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+// Constants
+const TWITTER_HANDLE = '@DebonairApe';
+const TWITTER_LINK = `https://twitter.com/DebonairApe`;
+const OPENSEA_COLLECTION_LINK =
+  'https://opensea.io/collection/debonairape';
+const TOTAL_MINT_COUNT = 1000;
+
+// @TODO: Note on contract redeploys
+// Step 1: Deploy the contract 'npx hardhat run scripts/deploy.js --network rinkeby'
+// Step 2: Update the 'CONTRACT_ADDRESS'
+// Step 3: Update the abi file(src/utils/MyEpicNFT.json)
+
+// Contract Address
+const CONTRACT_ADDRESS = '0x8beE30cfD19Fa1129f9c9F27F331aE71414BDEd3';
+
+const App = () => {
+  // Store our user's public wallet.
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccessful, setIsSuccessful] = useState(false);
+  const [mintCount, setMintCount] = useState(0);
+  const [openSeaLink, setOpenSeaLink] = useState(OPENSEA_COLLECTION_LINK);
+  const [etherScanLink, setEtherScanLink] = useState('');
+  const [isCopied, setIsCopied] = useState(false);
+
+  // Make sure this is async.
+  const checkIfWalletIsConnected = async () => {
+    // Make sure that you have access to window.ethereum
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    // Check if we're authorized to access the user's wallet.
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // User can have multiple authorized accounts.
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log('Connected to chain ' + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network.
+      const rinkebyChainId = '0x4';
+      if (chainId !== rinkebyChainId) {
+        console.log('You are not connected to the Rinkeby Test Network');
+      }
+
+      // Setup listener
+      setupEventListener();
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  // Implement our connectWallet method
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+      // Fancy method to request access to account
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log('Connected to chain ' + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network.
+      const rinkebyChainId = '0x4';
+      if (chainId !== rinkebyChainId) {
+        console.log('You are not connected to the Rinkeby Test Network');
+      }
+
+      // Setup listener
+      // This is for the case where a user comes to our site and connected
+      // their wallet for the first time.
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Our listener
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        connectedContract.on('NewEpicNFTMinted', (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          // alert(
+          //   `Hey there! We've minted your NFT. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          // );
+          setOpenSeaLink(
+            `https://opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+        console.log('Setup event listeners!');
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const askContractToMintNft = async () => {
+    setIsSuccessful(false);
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        // We always need these three things to communicate with the contract
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        console.log('Going to pop wallet now to pay gas...');
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        setIsLoading(true);
+
+        console.log('Mining... please wait.');
+        await nftTxn.wait();
+        setIsLoading(false);
+        setIsSuccessful(true);
+        getMintCount();
+
+        console.log(nftTxn);
+        setEtherScanLink(`https://etherscan.io/tx/${nftTxn.hash}`);
+        console.log(
+          `Mined, see transaction: https://etherscan.io/tx/${nftTxn.hash}`
+        );
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Set a limit on the # of minted NFTs
+  const getMintCount = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        let nftCount = await connectedContract.getTotalNFTsMintedCount();
+        console.log('NFTs minted: ', parseInt(nftCount, 10));
+        setMintCount(parseInt(nftCount, 10));
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getButton = () => {
+    if (currentAccount === '') {
+      return (
+        <button
+          onClick={connectWallet}
+          className="cta-button connect-wallet-button"
+        >
+          <FaWallet />
+          &nbsp; Connect Wallet
+        </button>
+      );
+    }
+    return (
+      <button
+        onClick={askContractToMintNft}
+        className="cta-button mint-button"
+        title="Mint an NFT"
+        disabled={isLoading || mintCount === TOTAL_MINT_COUNT}
+      >
+        <FaRegGem />
+        &nbsp;
+        {isLoading ? 'Mining...please wait!' : 'Mint NFT'}
+      </button>
+    );
+  };
+
+  const onCopyText = () => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  const navigateToOpenSea = () => {
+    window.open(OPENSEA_COLLECTION_LINK, '_blank');
+  };
+
+  // When the page loads
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    getMintCount();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <div className="header gradient-text ">
+            DEBUTANTE&nbsp;
+            <div className="gradient-text4">A</div>
+            <div className="gradient-text1">P</div>
+            <div className="gradient-text2">E</div>
+            <div className="gradient-text3">S</div>
+          </div>
+
+          <div className="bg-container">
+            <img
+              src={nftGif}
+              alt="NFT demo GIF"
+              className="nft-gif gif-bg-color "
+            />
+
+            {mintCount < TOTAL_MINT_COUNT && (
+              <p className="mint-count">
+                {mintCount} / {TOTAL_MINT_COUNT} claimed
+              </p>
+            )}
+            {mintCount === TOTAL_MINT_COUNT && (
+              <p className="mint-count">Sold out!</p>
+            )}
+            <p className="about">
+            D.A.P is a collection of 1,000 Debutante Ape NFTs – Non-generative,
+            unique 3D digital collectibles living on the Ethereum blockchain. 
+            Your Ape will grants access to members-only benefits, the first of
+            which is unlocking the exclusive access in the private Discord.
+            Future areas and perks can be unlocked by the community through 
+            roadmap activation.
+            </p>
+            {getButton()}
+            <button
+              onClick={navigateToOpenSea}
+              className="cta-button opensea-button"
+              title="View collection on OpenSea"
+            >
+              <FaThLarge />
+              &nbsp;  OpenSea
+            </button>
+            {currentAccount !== '' && (
+              <>
+                <p className="wallet-address-text">
+                  <div className="copy-action">
+                    <CopyToClipboard
+                      onCopy={onCopyText}
+                      text={currentAccount}
+                      title="Current wallet connected"
+                    >
+                      <span>{isCopied ? 'Copied!' : `${currentAccount}`}</span>
+                    </CopyToClipboard>
+                  </div>
+                </p>
+              </>
+            )}
+            {isSuccessful && (
+              <div className="success-message ">
+                <div className="success-message-color">
+                  <div className="wallet-checkmark"></div>
+                  <p>
+                    <FaCheck />
+                    &nbsp; Congratulations! You've just minted DAP NFT
+                  </p>
+                </div>
+                <p>
+                  View your NFT on&nbsp;
+                  <a
+                    className="openSea-link"
+                    href={openSeaLink}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    OpenSea
+                  </a>
+                </p>
+                <p>
+                  See your transaction on&nbsp;
+                  <a
+                    className="etherScan-link"
+                    href={etherScanLink}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Etherscan
+                  </a>
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{` ${TWITTER_HANDLE}`}   Copyright 2022</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
